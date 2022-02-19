addf9ea2302fadfaf585c2c253aaa4e26d7d89a1
Samuel Dubik <samuel.dubik@gmail.com>
2022-02-19 14:54:22 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'violet' : '#674baf',
+      'pink'   : '#e882e8',
+      'white'  : '#ffffff',
+    },
+
+    fontFamily: { 
+      'poppins'  : ['Poppins'],
+      'open'     : ['Open Sans'],
+    },
+    backgroundImage: {
+      'huddle-mobile' : "url('huddle-landing-page-with-single-introductory-section-master/images/bg-mobile.svg')",
+      'huddle-desktop': "url('huddle-landing-page-with-single-introductory-section-master/images/bg-desktop.svg')",
+    }
+  },
+  plugins: [],
+}
